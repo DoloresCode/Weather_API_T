c7f9d25751d8c7c76b7923b9d931567d95bc5cf2
Dolores Crazover <doloresnetweb@gmail.com>
2023-11-21 18:56:38 -0500

    cors installed for connection between front and backend

---
+++ Weather_API_Backend/server.js
@@ -0,0 +1,57 @@
+// Import Dependencies
+require("dotenv").config(); //it will load ENV variables from the file .env
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+// Create Express app
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// Set up middleware
+app.use(express.json());
+app.use(cors()); // this will allow requests from my frontend
+
+// Set up routes
+app.get("/", (req, res) => {
+    res.send("Hello, Dolores! Your Server is running...Better catch it!");
+})
+
+// Set Weather Endpoint "/weather"
+app.get("/weather", async (req, res) => {
+    const {city, country} = req.query;
+    const API_KEY = process.env.OPENWEATHER_API_KEY;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`;
+
+    try {
+        const response = await axios.get(url);
+        const data = response.data;
+        // console.log(data);
+
+    const result = {
+        location_name: `${data.name}, ${data.sys.country}`,
+        temperature: `${data.main.temp} °C`,
+        wind: `Speed: ${data.wind.speed} m/s, Direction: ${data.wind.deg} degrees`,
+        cloudiness: data.weather[0].description,
+        pressure: `${data.main.pressure} hpa`,
+        humidity: `${data.main.humidity}%`,
+        sunrise: new Date(data.sys.sunrise * 1000).toLocaleTimeString(),
+        sunset: new Date(data.sys.sunset * 1000).toLocaleTimeString(),
+        geo_coordinates: `[${data.coord.lat}, ${data.coord.lon}]`,
+        requested_time: new Date().toISOString(),
+        forecast: {} // this will add some forecast data ifthey are available
+    };
+
+    res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+// Test http://localhost:4000/weather?Bogota&country=co
+
+app.get("*", (req, res) => {
+    res.status(404).send("404 Not Found - This page does not exist");
+});
+
+// Server Listener
+app.listen(PORT, () => console.log(`Listening for client requests on port ${PORT}🔥🔥🔥🔥🔥🔥🔥`));
