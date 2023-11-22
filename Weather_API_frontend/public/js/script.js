da623afc310773fa3d0e64a58fe689ae52ebaddd
Dolores Crazover <doloresnetweb@gmail.com>
2023-11-21 23:30:34 -0500

    icones added for the results and css modified

--- Weather_API_frontend/public/js/script.js
+++ Weather_API_frontend/public/js/script.js
@@ -6,17 +6,17 @@ async function fetchWeatherInformation (event) {
     const response = await fetch(`http://localhost:4000/weather?city=${cityValue}&country=${countryValue}`);
     const result = await response.json();
     
-    document.getElementById('locationName').innerHTML = 'Location: ' + result.location_name;
-    document.getElementById('temperature').innerHTML = 'Temperature: ' + result.temperature;
-    document.getElementById('wind').innerHTML = 'Wind: ' + result.wind;
-    document.getElementById('cloudiness').innerHTML = 'Cloudiness: ' + result.cloudiness;
-    document.getElementById('pressure').innerHTML = 'Pressure: ' + result.pressure;
-    document.getElementById('humidity').innerHTML = 'Humidity: ' + result.humidity;
-    document.getElementById('sunrise').innerHTML = 'Sunrise: ' + result.sunrise;
-    document.getElementById('sunset').innerHTML = 'Sunset: ' + result.sunset;
-    document.getElementById('geoCoordinates').innerHTML = 'Geo Coordinates: ' + result.geo_coordinates;
-    document.getElementById('requestedTime').innerHTML = 'Requested Time: ' + result.requested_time;
-    document.getElementById('forecast').innerHTML = 'Forecast: ' + result.forecast;
+    document.getElementById('locationName').innerHTML = `<i class="fas fa-map-marker-alt"></i> Location: ${result.location_name}`;
+    document.getElementById('temperature').innerHTML = `<i class="fas fa-thermometer-half"></i> Temperature: ${result.temperature}`;
+    document.getElementById('wind').innerHTML = `<i class="fas fa-wind"></i> Wind: ${result.wind}`;
+    document.getElementById('cloudiness').innerHTML = `<i class="fas fa-cloud"></i> Cloudiness: ${result.cloudiness}`;
+    document.getElementById('pressure').innerHTML = `<i class="fas fa-tachometer-alt"></i> Pressure: ${result.pressure}`;
+    document.getElementById('humidity').innerHTML = `<i class="fas fa-tint"></i> Humidity: ${result.humidity}`;
+    document.getElementById('sunrise').innerHTML = `<i class="fas fa-sun"></i> Sunrise: ${result.sunrise}`;
+    document.getElementById('sunset').innerHTML = `<i class="fas fa-moon"></i> Sunset: ${result.sunset}`;
+    document.getElementById('geoCoordinates').innerHTML = `<i class="fas fa-globe"></i> Geo Coordinates: ${result.geo_coordinates}`;
+    document.getElementById('requestedTime').innerHTML = `<i class="fas fa-clock"></i> Requested Time: ${result.requested_time}`;
+    document.getElementById('forecast').innerHTML = `<i class="fas fa-calendar-alt"></i> Forecast: ${result.forecast}`;
 
     console.log(result);
 }
